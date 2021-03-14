79c9d4487eb086b7f3244ee78c0425497de647ac
yessinetrigui <55018185+yessinetrigui@users.noreply.github.com>
2021-03-14 04:08:00 +0100

    added the nav-bar / hbm / separateur

---
+++ public/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(() => {
+    //! ****** HBM *******/
+    $('#hbm').click(() => {
+        $('#hbm-nav').css({
+            visibility: 'visible',
+            animation: 'openHBM 2s forwards'
+        })
+    });
+    $('#close').click(() => {
+        $('#hbm-nav').css({
+            animation: 'closeHBM 1s forwards'
+        })
+    });
+});
